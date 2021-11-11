814b9c9c3ad1fbd78b3dcd59fffc4d966fc4a698
Aaron Sim <aaronsimkorea@hotmail.com>
2021-11-11 12:36:13 +0800

    adding front end display

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 from app import app
 from datetime import datetime
-from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+from flask_login import UserMixin , login_user , LoginManager, login_required, logout_user, current_user
 from app import login
+from flask import Flask, render_template, url_for, redirect , request
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+import sys
+import requests
+from flask_restful import Api, Resource , reqparse, abort,  fields, marshal_with
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///templates/database.db'
+
+
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+
+BASE = "http://127.0.0.1:5000/"
+
+
+api = Api(app)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -52,4 +75,25 @@ class Category(db.Model):
 
     def __repr__(self):
         return f"Post('{self.id}','{self.name}')"
+
+
+
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+
+@ app.route('/login', methods = ['GET','POST'])
+def login_page():
+
+	return render_template('static/loginPage.html')
+
+
+if __name__ == '__main__':
+	app.run(debug = True)
+
+
 
