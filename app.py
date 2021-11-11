84cbd9229e876c5878f017cfdc12be4d7cf1b9f2
Gabriel Soh <gabriel_soh_1997@hotmail.com>
2021-11-11 12:00:25 +0800

    Add load json

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 from app import app
+import json
+
+category = json.load(open('category.json'))
+expense = json.load(open('expense.json'))
+project = json.load(open('project.json'))
+user = json.load(open('user.json'))
